4885a98241af99151bb460ae2a01f9f68a12fd52
Everson OLiveira <82684218+Eor13@users.noreply.github.com>
2022-08-06 23:37:20 -0300

    Novo projeto de Cartões Pokémon

---
+++ js/index.js
@@ -0,0 +1,61 @@
+/*
+    step 1 - dar um jeito de pegar o eemento HTML da seta avançar
+
+OBJETIVO 1 - quando cicar na seta de avançar temos que mostrar o próximo cartão da sita
+    step 2 - da um jeito de identifacar o clique do usuario na seta avançar
+    step 3 - fazer aparecer o próximo cartão da lista
+    step 4 - buscar o cartão que esta selecionado e esconder
+
+OBJETIVO 2 - quando cicar na seta de votar temos que mostrar o próximo cartão da sita
+    step 2 - da um jeito de identifacar o clique do usuario na seta voltar
+    step 3 - fazer aparecer o  cartão anterior da lista
+    step 4 - buscar o cartão que esta selecionado e esconder
+
+*/
+
+
+//step 1 - dar um jeito de pegar o eemento HTML da seta avançar
+const btnAvançar = document.getElementById("avançar")
+const btnVoltar = document.getElementById("voltar")
+const cartoes = document.querySelectorAll(".cartao")
+let cartaoAtual = 0
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".select")
+    cartaoSelecionado.classList.remove("select")
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("select")
+}
+
+// ----------------------OBJETIVO 1
+//step 2 - da um jeito de identifacar o clique do usuario na seta avançar
+    btnAvançar.addEventListener("click",function(){
+
+        if(cartaoAtual ===cartoes.length-1) return  //clausula de segurança
+
+        // step 3 - fazer aparecer o próximo cartão da lista 
+        cartaoAtual++
+        mostrarCartao(cartaoAtual)
+
+        // step 4 - buscar o cartão que esta selecionado e esconder
+        esconderCartaoSelecionado()
+
+    })
+
+
+// ---------------------OBJETIVO 2
+
+//step 2 - da um jeito de identifacar o clique do usuario na seta avançar
+    btnVoltar.addEventListener("click",function(){
+
+        if(cartaoAtual ===0) return  //clausula de segurança
+
+        // step 3 - buscar o cartão que esta selecionado e esconder
+        esconderCartaoSelecionado()
+
+        // step 4 - fazer aparecer o  cartão anterior da lista
+        cartaoAtual--
+        mostrarCartao(cartaoAtual)
+    })
